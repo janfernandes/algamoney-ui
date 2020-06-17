6ac33b6dd9bc2f7f52cfd3bf459430ffaee81b82
Janayna <fernandesmjanayna@gmail.com>
2020-06-17 18:44:39 -0300

    aula 18.11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,13 +13,13 @@ import {PessoasPesquisaComponent} from './pessoas/pessoas-pesquisa/pessoas-pesqu
 import {PaginaNaoEncontradaComponent} from './core/pagina-nao-encontrada.component';
 
 const routes: Routes = [
-  // { path: '', redirectTo: 'lancamentos'},
+  { path: '', redirectTo: 'lancamentos', pathMatch: 'full'  },
   { path: 'lancamentos', component: LancamentosPesquisaComponent},
   { path: 'lancamentos/novo', component: LancamentoCadastroComponent},
   { path: 'lancamentos/:codigo', component: LancamentoCadastroComponent},
   { path: 'pessoas', component: PessoasPesquisaComponent},
   { path: 'pagina-nao-encontrada', component: PaginaNaoEncontradaComponent},
-  // { path: '**', redirectTo: 'pagina-nao-encontrada'},
+  { path: '**', redirectTo: 'pagina-nao-encontrada' }
 ];
 
 @NgModule({
