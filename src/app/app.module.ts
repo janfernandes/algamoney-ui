5e0cca8a6a08d4b50dbdaf53f86d02a15a2c790c
Janayna <fernandesmjanayna@gmail.com>
2020-06-17 19:17:16 -0300

    aula 18.13

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,7 @@ import {LancamentosModule} from './lancamentos/lancamentos.module';
 import {PessoasModule} from './pessoas/pessoas.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {RouterModule, Routes} from '@angular/router';
-import {LancamentosPesquisaComponent} from './lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component';
-import {LancamentoCadastroComponent} from './lancamentos/lancamento-cadastro/lancamento-cadastro.component';
-import {PessoasPesquisaComponent} from './pessoas/pessoas-pesquisa/pessoas-pesquisa.component';
-import {PaginaNaoEncontradaComponent} from './core/pagina-nao-encontrada.component';
-
-const routes: Routes = [
-  { path: '', redirectTo: 'lancamentos', pathMatch: 'full'  },
-  { path: 'lancamentos', component: LancamentosPesquisaComponent},
-  { path: 'lancamentos/novo', component: LancamentoCadastroComponent},
-  { path: 'lancamentos/:codigo', component: LancamentoCadastroComponent},
-  { path: 'pessoas', component: PessoasPesquisaComponent},
-  { path: 'pagina-nao-encontrada', component: PaginaNaoEncontradaComponent},
-  { path: '**', redirectTo: 'pagina-nao-encontrada' }
-];
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -33,7 +19,7 @@ const routes: Routes = [
     PessoasModule,
     CoreModule,
     HttpClientModule,
-    RouterModule.forRoot(routes)
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
